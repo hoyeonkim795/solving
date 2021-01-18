3d6d16a99e6fa7bc21598f327fe44a7fb46b26df
hoyeonkim <hy940313@gmail.com>
2021-01-18 14:54:23 +0900

    토네이도

--- samsung/마법사상어와토네이도.py
+++ samsung/마법사상어와토네이도.py
@@ -2,58 +2,72 @@ import sys
 sys.stdin = open("input.txt",'r')
 n = int(input())
 board = [[int(x) for x in input().split()] for _ in range(n)]
-r,c = n//2,n//2
 
-time =1
+ans = 0 # 격자 바깥으로 나간 모래의 양
 
+# 바람의 방향에 따른 모래 비율
+rate_left = [[0,0,2,0,0],[0,10,7,1,0],[5,'a',0,0,0],[0,10,7,1,0],[0,0,2,0,0]]
+rate_down = [[0,0,0,0,0],[0,1,0,1,0],[2,7,0,7,2],[0,10,'a',10,0],[0,0,5,0,0]]
+rate_right = [[0,0,2,0,0],[0,1,7,10,0],[0,0,0,'a',5],[0,1,7,10,0],[0,0,2,0,0]]
+rate_up = [[0,0,5,0,0],[0,10,'a',10,0],[2,7,0,7,2],[0,1,0,1,0],[0,0,0,0,0]]
+
+# 모래 이동
+def spread(ans,board,rate,nx,ny):
+    # Y의 좌표를 rate 배열의 좌표와 함께 대응할 수 있도록 좌표이동? 해준다.
+    a,b = nx-2, ny-2
+
+    temp = 0 # Y에서 빠져 나간 모래의 총 양
+    for i in range(5):
+        for j in range(5):
+            # 
+            if rate[i][j] != 'a' and rate[i][j] != 0:
+                if -1 < i+a < n and -1 < j+b < n:
+                    board[i+a][j+b] += board[nx][ny]*rate[i][j]//100
+                else:
+                    # 범위 안에 들어오지 않을 경우
+                    ans += board[nx][ny]*rate[i][j]//100
+                # a 자리에 모래를 채우기 위해서 빠져나가는 모래의 양을 temp 에 계속 더해준다.
+                temp += board[nx][ny]*rate[i][j]//100
+            # a 자리의 좌표를 기억
+            elif rate[i][j] == 'a':
+                remain = (i,j)
+    # 나머지 a 부분 처리
+    if -1 < remain[0]+a < n and -1 < remain[1]+b < n:
+        board[remain[0]+a][remain[1]+b] += board[nx][ny] - temp
+    else: # a 자리도 격자 바깥일 경우 격자 바깥으로 나가는 ans에 더해준다.
+        ans += board[nx][ny] - temp
+    # y 자리 0으로 초기화
+    board[nx][ny] = 0
+    return board, ans
+
+# 처음 시작 좌표
+x,y = n//2,n//2
+
+# 왼쪽 아래 오른쪽 위 
 dir = [(0,-1),(1,0),(0,1),(-1,0)]
-
-sand = [[(0,-2,5),(-1,-1,10),(-1,0,7),(-1,1,1),(-2,0,2),(1,-1,10),(1,0,7),(1,1,1),(2,0,2),(0,-1),(0,1)],
-[(1,1,10),(1,-1,10),(2,0,5),(-1,-1,1),(-1,1,1),(0,-2,2),(0,2,2),(0,-1,7),(0,1,7),(1,0),(-1,0)],
-[(0,2,5),(-1,0,7),(1,0,7),(-1,1,10),(-1,2,1),(-2,0,2),(1,1,10),(1,2,1),(2,0,2),(0,1),(0,-1,0)],
-[(0,-2,2),(0,2,2),(0,-1,7),(0,1,7),(-1,-1,10),(-1,1,10),(-2,0,5),(1,-1,1),(1,1,1),(-1,0),(1,0)]]
-
-
-
-def spread(x,y,r,c,dx,dy,m,result,board):
-    total = 0
-    if -1 < r+dx < n and -1 < c+dy < n: 
-        board[r+dx][c+dy] += board[x][y]*m // 100
-        total += board[x][y]*m // 100
-
-    else:
-        result += board[x][y]*m // 100
-        total += board[x][y]*m // 100
-
-    return board, result, total
-
-result = 0
-while (r,c) != (0,0):
+# 왼 아래 - 오른 오른 위 위 / 왼 왼 왼 아래 아래 아래 - 오른 오른 오른 오른 위 위 위 위
+# dir[1]일때랑 dir[3]일때 한 time 씩 늘어난다.
+time = 1
+flag = 0
+while flag != 1:
     for i in range(4):
-        for _ in range(time):
-            r,c = r+dir[i][0], c+dir[i][1]
-            x,y = r+sand[i][10][0],c+sand[i][10][1]
-            a,b = r+sand[i][9][0],c+sand[i][9][1]
-            sub = 0
-            for s in range(9):
-                dx,dy,m = sand[i][s][0],sand[i][s][1],sand[i][s][2]
-                board, result, total = spread(x,y,r,c,dx,dy,m,result,board)
-                sub += total
-            if -1< a < n and -1 < b < n:
-                board[a][b] += board[x][y] - sub
-            else:
-                result += board[x][y] - sub
-            board[x][y] = 0
-            print(r,c)
-            print(board)
-            print(result)
-            if (r,c) == (0,0):
-                break 
-        if (r,c) == (0,0):
-            break
-        if i == 1 or i == 3:
+        dx, dy = dir[i]
+        for j in range(time):
+            x, y = x+dx, y+dy
+            if i == 0: # 왼쪽
+                board, ans = spread(ans,board,rate_left,x,y)
+            elif i == 1: # 아래
+                board, ans = spread(ans,board,rate_down,x,y)
+            elif i == 2: # 오른쪽
+                board, ans = spread(ans,board,rate_right,x,y)
+            elif i == 3: # 위에
+                board, ans = spread(ans,board,rate_up,x,y)
+
+            if (x, y) == (0,0): # 0,0 으로 왔을때 종료
+                flag = 1
+                break
+        if i == 1 or i == 3: # 1,3 일때 time 늘려주기
             time += 1
-
-
-
-print(result)
+        if flag == 1:
+            print(ans)
+            break
--- samsung/어른상어.py
+++ samsung/어른상어.py
@@ -1,9 +1,81 @@
 import sys
-sys.stdin = open("input.txt",'r')
+sys.stdin = open('input.txt','r')
 from copy import deepcopy
-n, m, K = map(int,input().split())
-board = [[int(x) for x in input().split()]for _ in range(n)]
+n, m, k = map(int,input().split())
+# 상어의 번호를 담느 격자
+board = [[int(x) for x in input().split()] for _ in range(n)]
+# 냄새 영역 표시
+# [0,-1] 의 의미는 냄새가 0 이고 -1 은 그 냄새 뿌린 상어의 번호가 올자리
+smell = [[[0,-1]]*n for _ in range(n)]
+# 각 상어의 방향과 우선순위 정보를 담는 ARR
 shark_dir = list(map(int,input().split()))
-shark = [[[int(x) for x in input().split()] for _ in range(4)] for _ in range(m)]
-
+for i in range(m):
+    shark_dir[i] = [shark_dir[i]]
+    for _ in range(4):
+        shark_dir[i].append(list(map(int,input().split())))
+# shark_dir = [방향, [우선순위들...] ]
+# print(shark_dir)
+cnt = 0
 dir = [(-1,0),(1,0),(0,-1),(0,1)]
+temp = 0
+while True:
+    if temp == m-1 : # 1개만 남았을 경우 BREAK
+        print(cnt)
+        break
+    if cnt >= 1000:
+        print('-1')
+        break
+
+    # 현재 상어가 위치한 자리에 냄새를 뿌린다
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] != 0:
+                smell[i][j] = [k,board[i][j]]
+
+    new_board = deepcopy(board)
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] != 0:
+                d, up, down, left, right = shark_dir[board[i][j]-1]
+                dir_arr = [up, down, left, right]
+                flag = 0
+                for p in range(4):
+                    dx, dy = dir[dir_arr[d-1][p] - 1]
+                    # 인접한 영역에 냄새가 0 인 경우 우선순위를 고려
+                    if -1 < i+dx < n and -1 < j+dy < n and smell[i+dx][j+dy] == [0,-1]:
+                        # 이동하는 자리에 어떠한 상어도 없을경우
+                        if new_board[i+dx][j+dy] == 0:
+                            new_board[i+dx][j+dy] = board[i][j]
+                            new_board[i][j] = 0
+                        # 이동하는 자리에 다른 상어가 있을경우 넘버를 비교해서 작은것을 위치시키고 temp 를 +1 시킨다 하나 격자밖으로 쫓아내지니까
+                        else:
+                            if new_board[i+dx][j+dy] > board[i][j]:
+                                new_board[i+dx][j+dy] = board[i][j]
+                            temp += 1
+                            new_board[i][j] = 0
+                        shark_dir[board[i][j]-1][0] = dir_arr[d-1][p]
+                        flag = 1
+                        break
+                else:
+                    # 인접한 영역에 냄새가 0 인 곳이 없을 경우
+                    for p in range(4):
+                        dx, dy = dir[dir_arr[d-1][p] - 1]
+                        # smell[i+dx][j+dy][1]가 자신의 넘버와 같은 경우 즉 자기의 냄새인 영역
+                        if -1 < i+dx < n and -1 < j+dy < n and smell[i+dx][j+dy][1] == board[i][j]:
+                            new_board[i+dx][j+dy] = board[i][j]
+                            new_board[i][j] = 0
+                            shark_dir[board[i][j]-1][0] = dir_arr[d-1][p]
+                            break
+
+    board = deepcopy(new_board)
+    # 그리고 냄새 영역 이동했으니까 하나씩 줄여주기
+    for i in range(n):
+        for j in range(n):
+            if smell[i][j][1] != -1:
+                smell[i][j][0] -= 1
+                if smell[i][j][0] == 0:
+                    smell[i][j] = [0,-1]
+                            
+    # print(board)
+    # print(smell)
+    cnt += 1
