d7182f289584ed77cb8cafc6ac004325ce8a86ee
hoyeonkim <hy940313@gmail.com>
2021-01-22 18:22:36 +0900

    0122

--- samsung/모노미노도미노2.py
+++ samsung/모노미노도미노2.py
@@ -7,9 +7,7 @@ red_green = [[0]*4 for _ in range(10)]
 
 # 파란 보드
 red_blue = [[0]*10 for _ in range(4)]
-# print(red)
-# print(green)
-# print(blue)
+
 
 # 빨간 + 초록 
 # 빨강의 경계선(행) 까지 간다음에.. 끝까지..
@@ -26,35 +24,90 @@ red_blue = [[0]*10 for _ in range(4)]
 # 4칸 다 찾는지 확인
     # 지우기 만들기
 
+def green_remove(red_green,ans):
+    while True:
+        for i in range(10):
+            if red_green[i] == [1,1,1,1]:
+                ans += 1
+                red_green[i] = [0,0,0,0]
+                for x in range(i,0,-1):
+                    red_green[x] = red_green[x-1]
+                red_green[0] = [0,0,0,0]
+        else:
+            break
+
+    return red_green, ans
+
+def blue_remove(red_blue,ans):
+    while True:
+        for j in range(10):
+            if (red_blue[0][j],red_blue[1][j],red_blue[2][j],red_blue[3][j]) == (1,1,1,1):
+                ans += 1
+                red_blue[0][j],red_blue[1][j],red_blue[2][j],red_blue[3][j] = 0,0,0,0
+                for y in range(j,0,-1):
+                    red_blue[0][y],red_blue[1][y],red_blue[2][y],red_blue[3][y] = red_blue[0][y-1],red_blue[1][y-1],red_blue[2][y-1],red_blue[3][y-1]
+        else:
+            break
+    return red_blue, ans
+
+def blue_special(red_blue):
+    cnt = 0
+    for i in range(4):
+        if red_blue[i][4] == 1 or red_blue[i][5] == 1:
+            cnt += 1
+            break
+
+    if cnt > 0:
+        while True:
+            if  (red_blue[0][4],red_blue[1][4],red_blue[2][4],red_blue[3][4]) == (0,0,0,0) and (red_blue[0][5],red_blue[1][5],red_blue[2][5],red_blue[3][5]) == (0,0,0,0):
+                break
+
+            for j in range(9,4,-1):
+
+                red_blue[0][j],red_blue[1][j],red_blue[2][j],red_blue[3][j] = red_blue[0][j-1],red_blue[1][j-1],red_blue[2][j-1],red_blue[3][j-1]
+            red_blue[0][4],red_blue[1][4],red_blue[2][4],red_blue[3][4] = 0,0,0,0
+
+    return red_blue
+
+def green_special(red_green):
+    cnt = 0
+    for j in range(4):
+        if red_green[4][j] == 1 or red_green[5][j] == 1:
+            cnt += 1
+            break
+    if cnt > 0:
+        while True:
+            if red_green[4] == [0,0,0,0] and red_green[5] == [0,0,0,0]:
+                break
+            for i in range(9,4,-1):
+                red_green[i] = red_green[i-1]
+            red_green[4] = [0,0,0,0]
+    return red_green
 
 
+ans = 0
 for _ in range(n):
     t,x,y = map(int,input().split())
-    if t == 1: # 세로
-        info = [[x,y]]
-    elif t == 2: # 가로
-        info = [[x,y],[x,y+1]]
-    elif t == 3: # 세로
-        info = [[x,y],[x+1,y]]
-    
     
     if t == 1:
         # 빨강 + 초록
         nx, ny = x,y
         for i in range(1,10-x):
+
             if red_green[x+i][y] == 0:
-                nx, ny = x+i,y
+                nx, ny = x+i, y
             else:
-                nx, ny = x-1+i,y
+                nx, ny = x-1+i, y
                 break
         red_green[nx][ny] = 1
         # 빨강 + 파랑
         nx, ny = x,y
         for j in range(1,10-y):
-            if red_blue[x][y+i] == 0:
-                nx, ny = x,y+i
+            if red_blue[x][y+j] == 0:
+                nx, ny = x,y+j
             else:
-                nx, ny = x,y-1+i
+                nx, ny = x,y-1+j
+                break
         red_blue[nx][ny] = 1
     elif t == 2:
         nx, ny = x, y
@@ -73,18 +126,18 @@ for _ in range(n):
         # 빨강 + 파랑
         nx, ny = x, y
         a, b = x, y+1
-        for i in range(1,9-y):
-            if red_blue[x][y+i] == 0 and red_blue[x][y+i+1] == 0:
-                nx, ny = x, y+i
-                a, b = x, y+1+i
+        for j in range(1,9-y):
+            if red_blue[x][y+j] == 0 and red_blue[x][y+j+1] == 0:
+                nx, ny = x, y+j
+                a, b = x, y+1+j
             else:
-                nx, ny = x, y+i-1
-                a, b = x, y+i
+                nx, ny = x, y+j-1
+                a, b = x, y+j
                 break
         red_blue[nx][ny] = 1
         red_blue[a][b] = 1
-# (2, 2), (3, 2)와 (2, 3), (3, 3)
-    elif t == 3:
+
+    elif t == 3: # info = [[x,y],[x+1,y]]
         nx, ny = x+1,y
         for i in range(1,9-x):
             if red_green[x+1+i][y] == 0 and red_green[x+i][y] == 0:
@@ -95,19 +148,30 @@ for _ in range(n):
         red_green[nx][ny] = 1
         red_green[nx-1][ny] = 1
 
-        nx, ny = x,y
+        nx, ny = x+1,y
         for j in range(1,10-y):
-            if red_blue[x+1][y+i] == 0 and red_blue[x][y+i] == 0:
-                nx, ny = x,y+i
+            if red_blue[x][y+j] == 0 and red_blue[x+1][y+j] == 0:
+                nx, ny = x+1,y+j
             else:
-                nx, ny = x,y-1+i
+                nx, ny = x+1,y-1+j
+                break
+            
         red_blue[nx][ny] = 1
         red_blue[nx-1][ny] = 1
 
-print(red_blue)
-print(red_green)
+    red_blue,ans = blue_remove(red_blue,ans)
+    red_green,ans = green_remove(red_green,ans)
+    red_blue = blue_special(red_blue)
+    red_green = green_special(red_green)
 
+# print(red_blue)
+# print(red_green)
 
-
-
-
+print(ans)
+total = 0
+# 초록색
+for i in range(6,10):
+    total += sum(red_green[i])
+for i in range(4):
+    total += sum(red_blue[i])
+print(total)
